873621eeaaad80faa4b5a4fc8edde1ccc854d6bb
Rowizar <114011598+Rowizar@users.noreply.github.com>
2023-06-29 14:22:26 +0300

    Start_1%_

---
+++ qa_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+class QuestionRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+class AnswerRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+class UserCategoryPreference(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    preference_level = models.IntegerField()
+from django.db import models
+
+# Create your models here.
