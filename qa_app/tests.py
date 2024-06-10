d9fb330dd29fd74b842b6e9ce34d0c3c008a9333
Rowizar <114011598+Rowizar@users.noreply.github.com>
2024-06-10 19:50:28 +0300

    First_part_Forum_V4-100%

--- qa_app/tests.py
+++ qa_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Question
 
-# Create your tests here.
+class QuestionTests(TestCase):
+    fixtures = ['initial_data.json']
+
+    def test_question_list(self):
+        response = self.client.get(reverse('feed'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Sample Question")
+
+    def test_question_detail(self):
+        question = Question.objects.get(pk=42)
+        response = self.client.get(reverse('question_detail', args=[question.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "This is a sample question text.")
